4b72ff9132143a84ac3e7a6104bd91a04194ad5e
Frank Luis Ravelo <frankluisravelo@Franks-MBP.cable.rcn.com>
2020-01-26 20:45:49 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,14 @@
-let cardOne = "queen";
-let cardTWo = "king";
-let cardThree = "queen";
-let cardFour = "king";
+
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+let cardOne = cards[0];
+let cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) ;
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+    } else {
+      alert("Sorry, try again");
+    }
