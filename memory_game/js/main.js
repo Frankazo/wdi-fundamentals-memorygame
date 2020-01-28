7dc4691d3172a70f84eedf40a8c265c8d714b164
Frank Luis Ravelo <frankluisravelo@Franks-MBP.cable.rcn.com>
2020-01-28 07:24:46 -0500

    add object

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,17 @@
 
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{ rank: 'queen',
+  suit: 'hearts',
+  cardImage: 'images/queen-of-hearts.png'},
+{ rank: 'queen',
+  suit: 'diamonds',
+  cardImage: 'images/queen-of-diamonds.png'},
+{ rank: 'king',
+  suit: 'hearts',
+  cardImage: 'images/king-of-hearts.png'},
+{ rank: 'king',
+  suit: 'diamonds',
+  cardImage: 'images/king-of-diamonds.png'}];
 const cardsInPlay = [];
 
 function checkForMatch (){
@@ -12,9 +24,10 @@ function checkForMatch (){
 
 
 function flipCard(cardId){
-  console.log("User flipped " + cards[cardId]);
-
-  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
+  console.log(cards[cardId].suit);
+  console.log(cards[cardId].cardImage);
+  cardsInPlay.push(cards[cardId].rank);
 
   if (cardsInPlay.length === 2) {
       checkForMatch();
@@ -25,4 +38,4 @@ function flipCard(cardId){
 
 
 flipCard(0);
-flipCard(2);
+flipCard(1);
