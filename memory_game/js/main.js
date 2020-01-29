4de836e369aff5b52c81381d1aadecf7ccd05590
Frank Luis Ravelo <frankluisravelo@Franks-MBP.cable.rcn.com>
2020-01-29 11:21:33 -0500

    adding DOM manipulation

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -12,30 +12,29 @@ const cards = [
 { rank: 'king',
   suit: 'diamonds',
   cardImage: 'images/king-of-diamonds.png'}];
-const cardsInPlay = [];
+let cardsInPlay = [];
 
 function checkForMatch (){
   if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
+  alert("You found a match!");
 } else {
-  console.log("Sorry, try again.");
-}
-}
-
-
-function flipCard(cardId){
-  console.log("User flipped " + cards[cardId].rank);
-  console.log(cards[cardId].suit);
-  console.log(cards[cardId].cardImage);
+  alert("Sorry, try again.");
+}};
+function flipCard(){
+  let cardId = this.getAttribute("data-id");
+  this.setAttribute("src", cards[cardId].cardImage);
   cardsInPlay.push(cards[cardId].rank);
-
   if (cardsInPlay.length === 2) {
       checkForMatch();
       return;
       }
-
-}
-
-
-flipCard(0);
-flipCard(1);
+};
+function createBoard(){
+  for (let i = 0; i < cards.length; i++) {
+    let cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    document.getElementById('game-board').appendChild(cardElement);
+    cardElement.addEventListener('click', flipCard);
+}};
+createBoard();
